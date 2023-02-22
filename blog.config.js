b3b62b4f23e176876ca57f87eaa985cd7657e2f2
Jxxxxx04 <122721123+Jxxxxx04@users.noreply.github.com>
2023-02-22 14:30:51 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -12,7 +12,7 @@ const BLOG = {
 
   AUTHOR: '阿芜', // 您的昵称 例如 tangly1024
   BIO: '喜欢写无聊日记，努力做个人', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion-next-apmk1f9ci-jxxxxx04.vercel.app', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: '', // 邮箱地址 例如mail@tangly1024.com
